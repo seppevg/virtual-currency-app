ebdd8a25b04b5a0b6cde6caaf3f537f92429e830
Seppe Vangeel <seppe.vg@live.be>
2021-04-21 12:17:50 +0200

    fix: change function name in api.js

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 const coinController = require('../controllers/coins');
 
-router.get('/', coinController.getHome);
+router.get('/', coinController.getLoggedIn);
 
 module.exports = router;
