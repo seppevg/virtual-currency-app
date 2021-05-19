7b8b5c722dc3dfd8b91791d15e8981a6923511af
Seppe Vangeel <seppe.vg@live.be>
2021-05-19 20:48:51 +0200

    feat: add second functionality to gulpfile.js, which consits of minifyImg

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,28 @@ const { watch, series } = require('gulp');
 const gulp = require("gulp");
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
+const imagemin = require("gulp-imagemin");
 const sync = require("browser-sync").create();
 
 function sassToCss() {
     return src('./public/stylesheets/sass/app.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest("./public/stylesheets/dist"));
+        .pipe(gulp.dest('./public/stylesheets/dist'));
 }
 
+function minifyImg() {
+    return gulp
+        .src('./bigImages/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./public/images'));
+}
+
+//Zoekt naar nieuwe bestanden in de bigImages map
+gulp.task("minifyImg", () => {
+    gulp.watch('./bigImages/*', minifyImg);
+});
+
+//Zoekt naar veranderingen in alle sass (scss) files
 exports.start = () => {
     watch('./public/stylesheets/sass/**/*.scss', sassToCss);
 };
