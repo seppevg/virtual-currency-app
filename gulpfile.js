01bb88cbd595b5016ea7c2e69d761ea10bbd4df6
Seppe Vangeel <seppe.vg@live.be>
2021-05-19 21:28:21 +0200

    feat: add third functionality to gulp-flow which is to minify the css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-const { src, dest } = require("gulp");
-const { watch, series } = require('gulp');
+const { src, dest, watch } = require("gulp");
 
 const gulp = require("gulp");
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 const imagemin = require("gulp-imagemin");
-const sync = require("browser-sync").create();
+const cleanCSS = require('gulp-clean-css');
 
 function sassToCss() {
-    return src('./public/stylesheets/sass/app.scss')
+    return gulp
+        .src('./public/stylesheets/sass/app.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./public/stylesheets/dist'));
 }
@@ -20,6 +20,13 @@ function minifyImg() {
         .pipe(gulp.dest('./public/images'));
 }
 
+function minifyCss() {
+    return gulp
+        .src('./public/stylesheets/dist/app.css')
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./public/stylesheets/clean_css'));
+}
+
 //Zoekt naar nieuwe bestanden in de bigImages map
 gulp.task("minifyImg", () => {
     gulp.watch('./bigImages/*', minifyImg);
@@ -28,4 +35,6 @@ gulp.task("minifyImg", () => {
 //Zoekt naar veranderingen in alle sass (scss) files
 exports.start = () => {
     watch('./public/stylesheets/sass/**/*.scss', sassToCss);
+    gulp.watch('./bigImages/*', minifyImg);
+    gulp.watch('./public/stylesheets/dist/app.css', minifyCss);
 };
